5f59d5dac0331851d75959f65a6b0f3d9e0dc89b
eldonleka <eldonleka@gmail.com>
2026-02-01 18:46:09 +0100

    all

--- sandri/v1/js/qr.js
+++
@@ -1,7 +0,0 @@
-const scanner=new Html5Qrcode("qr-reader")
-
-scanner.start({facingMode:"environment"},{fps:10,qrbox:250},
-txt=>{
-if(import.style.display==="block") qr.value=txt
-if(sales.style.display==="block") saleQR.value=txt
-})
